bee78ac1ac466b55e48fa950cdd9747eb85cc9d7
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2023-11-05 17:31:12 -0700

    Simplify the code that gets the config file path
    This also fixes a bug where the code would return an empty string for
    module paths longer than 260 characters.

--- src/cFileHelper.cpp
+++ src/cFileHelper.cpp
@@ -1,40 +1,19 @@
 #include "cFileHelper.h"
 
 #include <Windows.h>
+#include "wil/resource.h"
+#include "wil/filesystem.h"
 
-// Based off: https://stackoverflow.com/a/875264
-std::wstring cFileHelper::GetCurrentPath()
+std::filesystem::path cFileHelper::GetCurrentModuleDirectory()
 {
-    WCHAR buffer[MAX_PATH] = { 0 };
-    GetModuleFileName(NULL, buffer, MAX_PATH);
-    const std::wstring fullPath = std::wstring(buffer);
+    wil::unique_cotaskmem_string modulePath = wil::GetModuleFileNameW(wil::GetModuleInstanceHandle());
 
-    const std::wstring::size_type lastDirSeparatorPos = fullPath.find_last_of(L'\\/');
-    if (lastDirSeparatorPos != std::wstring::npos)
-    {
-        return fullPath.substr(0, lastDirSeparatorPos);
-    }
-    else
-    {
-        return fullPath;
-    }
+    std::filesystem::path temp(modulePath.get());
+
+    return temp.parent_path();
 }
 
-std::wstring cFileHelper::GetParentDirectory(std::wstring path)
-{
-    // Warning: We are going to make a lot of assumptions about the format of the path
-    // this works for us but will not be robust enough to be used in many other applications
-
-    // Strip any leading directory seperator 
-    if (path.back() == '\\')
-    {
-        path.erase(path.size() - 1, 1);
-    }
-
-    return path.substr(0, path.find_last_of('\\'));
-}
-
-bool cFileHelper::DoesFileExist(const std::wstring& path)
+bool cFileHelper::DoesFileExist(const std::filesystem::path& path)
 {
     const DWORD fileType = GetFileAttributes(path.c_str());
 
